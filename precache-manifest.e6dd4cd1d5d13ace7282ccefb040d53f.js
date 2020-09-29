f31f22554b400b99798556f4ce655c5b85e64665
parkdl <ehddlf2395@gmail.com>
2020-09-29 17:03:42 +0900

    Updates

---
+++ precache-manifest.e6dd4cd1d5d13ace7282ccefb040d53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cd7e0d2d47bd3b5fd7c41c60ccd167",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "2f0cc5481eb365789d38",
+    "url": "/habit-tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit-tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit-tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0cc5481eb365789d38",
+    "url": "/habit-tracker/static/js/main.a78b6139.chunk.js"
+  },
+  {
+    "revision": "5d759b3fde7c32f59156",
+    "url": "/habit-tracker/static/js/runtime-main.0e3ad41c.js"
+  }
+]);
